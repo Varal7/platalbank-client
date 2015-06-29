e8e6d83289701aadc3a3f8900746fffa58e7e006
Nadrieril <nadrieril@gmail.com>
2015-06-29 15:56:37 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name platalbankClientApp
+ * @description
+ * # platalbankClientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('platalbankClientApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name platalbankClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the platalbankClientApp
+ */
+angular.module('platalbankClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
