0f4605df1f925ef3e61256e739c821999713ddb4
Ekleog <LeoG@spard.me>
2015-07-11 02:16:09 +0200

    Show / hide operation details on click

--- src/app/components/transactionunit/transactionunit.controller.js
+++ src/app/components/transactionunit/transactionunit.controller.js
@@ -6,7 +6,17 @@
     .controller('TransactionunitController', TransactionunitController);
 
   /** @ngInject */
-  function TransactionunitController() {
+  function TransactionunitController($scope) {
     var _this = this;
+    $scope.clickMe = function(event) {
+      console.log(event);
+      if (event.target.classList.contains("hide")) {
+        event.target.classList.remove("hide");
+        event.target.classList.add("show");
+      } else {
+        event.target.classList.remove("show");
+        event.target.classList.add("hide");
+      }
+    }
   }
 })();
