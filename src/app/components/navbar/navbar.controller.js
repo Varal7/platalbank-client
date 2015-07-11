0363c95c9ac114f57ee9cad1f8c376fe5ecd1340
Varal7 <varaal@gmail.com>
2015-07-11 04:22:33 +0200

    Added interactivity to NavBar

--- src/app/components/navbar/navbar.controller.js
+++ src/app/components/navbar/navbar.controller.js
@@ -6,11 +6,25 @@
     .controller('NavbarController', NavBarController);
 
   /** @ngInject */
-  function NavBarController() {
+  function NavBarController($scope,$mdDialog) {
     var _this = this;
 
     _this.binets = [{name: 'BR', balance:'2000', logo:''},
                     {name:'BB', balance:'20', logo:''}];
+    _this.showBinets = true;
+  $scope.navigateTo = function(to, event) {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .title('Navigating')
+          .content('Imagine being taken to ' + to)
+          .ariaLabel('Navigation demo')
+          .ok('Neat!')
+          .targetEvent(event)
+      );
+    }
+    $scope.toggleDisplay = function(event){
+      _this.showBinets=!_this.showBinets;
+    }
 
   }
 })();
