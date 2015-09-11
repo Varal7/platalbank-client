61b2c807abfd1571deb79aa9077f860691f813bc
Nocty-chan <laetitia.shao@gmail.com>
2015-09-11 21:10:38 +0200

    Balance preview
    Set disposition of elements,
    Cleaned scss file
    Added balance preview

--- src/app/components/userInfo/userinfo.controller.js
+++ src/app/components/userInfo/userinfo.controller.js
@@ -8,18 +8,16 @@
   /** @ngInject */
   function userInfoController($mdDialog) {
     var _this = this;
-    _this.user = {name:"Louis", surname:"Vaneau", shortname:"LVN", balance:"345", photo:"../assets/images/conscrit.jpg"};
+    _this.user = {name:"Louis", surname:"Vaneau", shortname:"LVN", preview: "312", balance:"345", photo:"../assets/images/conscrit.jpg"};
    _this.navigateTo = function(to, event) {
       $mdDialog.show(
         $mdDialog.alert()
           .title('Navigating')
           .content('Imagine being taken to ' + to)
           .ariaLabel('Navigation demo')
-          .ok('Neat!')
-          
+          .ok('Neat!')        
       );
     }
   }
-
 })();
 
